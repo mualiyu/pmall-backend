d57918454ccc31a4516da8cce60a8897ed96a9a9
Muktar Usman <mualiyuoox@gmail.com>
2024-02-25 15:34:22 +0100

    Added product management

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -2,15 +2,15 @@
 # Welcome to Pmall
 {{-- # Email Verification --}}
 
-Thank you for signing up.
+{{-- Thank you for signing up. --}}
 {{-- Use this link to verify your email <a href="{{url('/tokens?token='.$pin)}}">{{url('/tokens?token='.$pin)}}</a> --}}
 
-your password is {{$pass}}
+Your password is {{$pass}}
 
 {{-- <x-mail::button :url="''">
 Button Text
 </x-mail::button> --}}
 
-Thank you for signing up with Pmall.<br>
+Thank you for registering with Pmall.<br>
 {{ config('app.name') }}
 </x-mail::message>
